8c11aa8e34728226804164a2829e8bd7c26d12f5
Ratnam Yadav <ratnam.yadav@icloud.com>
2017-06-01 05:24:34 +0530

    small changes

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,8 +3,8 @@ class Company < ApplicationRecord
     
   validates :contact, length: {minimum: 9, maximum: 9}, if: :contact_present?
 
-  has_many :revenues
-  has_many :deals
+  has_many :revenues, dependent: :destroy
+  has_many :deals, dependent: :destroy
 
   def calculate_month_expenses(month, year)
     
